208ee5e1724e8a72c4b4bff816258a6817d66951
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-26 17:45:17 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,12 @@
 
 def display_rainbow(colors)
   puts colors[0][0].upcase + ": " + colors[0]
+  puts colors[0][0].upcase + ": " + colors[1]
+  puts colors[0][0].upcase + ": " + colors[2]
+  puts colors[0][0].upcase + ": " + colors[3]
+  puts colors[0][0].upcase + ": " + colors[4]
+  puts colors[0][0].upcase + ": " + colors[5]
+  puts colors[0][0].upcase + ": " + colors[6]
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
